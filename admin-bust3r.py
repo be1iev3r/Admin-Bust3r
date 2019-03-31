6bc82aa1d3ea8d5a3aae4a18db5bc9b8c9734987
root <root@localhost.localdomain>
2019-03-31 14:51:53 +0530

    Updated features

--- admin-bust3r.py
+++ admin-bust3r.py
@@ -11,34 +11,51 @@ import threading
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
-parser = argparse.ArgumentParser(description="A powerful and advanced admin panel buster tool in python. ")
+# Argument Parser
+example_text = "example: python3 admin-bust3r.py -u http://example.com -f -e php -o <outfile>"
+
+parser = argparse.ArgumentParser(description=" A powerful,fast and advanced admin panel buster tool in python. ", epilog = example_text)
 parser.add_argument("-u", "--url", help=" Target URL to check for admin panel. ", dest="target")
-parser.add_argument("-e", "--ext", help=" To check against specific extension only. ", dest="ext")
+parser.add_argument("-e", "--ext", help=" To check against specific extension only. EX:- (php , asp, html, aspx) ", dest="ext")
 parser.add_argument("-s", "--small", help=" To check against small wordlist (default = medium). ", action="store_true", default=False, dest="small")
 parser.add_argument("-l", "--large", help=" To check against large wordlist (default = medium). ", action="store_true", default=False, dest="large")
-parser.add_argument("-vl", "--verylarge", help=" To check against very large wordlist (Not Recommended). ", action="store_true", default=False, dest="verylarge")
+parser.add_argument("-vl", "--verylarge", help=" To check against very large wordlist (default = medium). ", action="store_true", default=False, dest="verylarge")
 parser.add_argument("-f", "--fast", help=" For Faster Scaning. ", action="store_true", default=False, dest="fast")
 parser.add_argument("-o", "--output", help=" To save output in a file. ", dest="outfile")
 
 args = parser.parse_args()
 
-# Banner
-#########################
-#########################
-#########################
+# Print the Banner
+banner = '''
+     ___       _______  .___  ___.  __  .__   __.        .______    __    __       _______.___________.____   .______      
+    /   \     |       \ |   \/   | |  | |  \ |  |        |   _  \  |  |  |  |     /       |           |___ \  |   _  \     
+   /  ^  \    |  .--.  ||  \  /  | |  | |   \|  |  ______|  |_)  | |  |  |  |    |   (----`---|  |----` __) | |  |_)  |    
+  /  /_\  \   |  |  |  ||  |\/|  | |  | |  . `  | |______|   _  <  |  |  |  |     \   \       |  |     |__ <  |      /     
+ /  _____  \  |  '--'  ||  |  |  | |  | |  |\   |        |  |_)  | |  `--'  | .----)   |      |  |     ___) | |  |\  \----.
+/__/     \__\ |_______/ |__|  |__| |__| |__| \__|        |______/   \______/  |_______/       |__|    |____/  | _| `._____| 
+'''
+print(Fore.YELLOW + Style.BRIGHT + banner)
+print(Fore.BLUE + Style.BRIGHT + "\t\t\t\t\t\t\t v-1.0 Made by:-" + Fore.GREEN + Style.BRIGHT + " @be1iev3r \n")
 
-target = args.target
+# Disclosure
+print(Fore.GREEN + "\t\t\t\t A powerful,fast and advanced admin panel buster tool in python.\n ")
+print(Fore.YELLOW + "[!] Author is not responsible or involved in any kind of your activity. Use of this tool in illegal activity is full on user's responsibilty. Happy Hacking!! \n") 
 
 # Check if the target host is reachable
+target = args.target
+
+if target[-1] == "/":
+    target = target[:-1]
 try:
     req = requests.get(target)
 except:
     print(Back.RED + "[-] Target host not reachable.")
-    print(Fore.YELLOW + "EXITING NOW...")
+    print(Fore.YELLOW + "[!] EXITING NOW...")
     quit()
 
 # Check for robots.txt
 try:
+    print(Fore.BLUE + "[*] Searching for 'robots.txt'...")
     req = requests.get(target + '/robots.txt', timeout=10)
     if req.status_code == 200:
         print(Fore.GREEN + "[+] 'robots.txt' found. Contents of 'robots.txt' are :")
@@ -71,13 +88,13 @@ else:
 urls_list = []
 ext = args.ext
 if ext:
-    ext_list =['php','asp','html']
+    ext_list =['php','asp','html','aspx']
     if ext not in ext_list:
         print(Back.RED + "[-] Invalid extension.")
-        print(Fore.YELLOW + "EXITING NOW...")
+        print(Fore.YELLOW + "[!] EXITING NOW...")
         quit()
     else:
-        new_ext_list = [item for item in ex_list if item!=ext]
+        new_ext_list = [item for item in ext_list if item!=ext]
     try:
         with open(filename) as paths:
             for path in paths:
@@ -88,7 +105,7 @@ if ext:
                     urls_list.append(path)
     except IOError:
         print(Back.RED + "[-] Error opening wordlist file. Please check if it is downloaded while git cloning.")
-        print(Fore.YELLOW + "EXITING NOW...")
+        print(Fore.YELLOW + "[!] EXITING NOW...")
         quit()
 else:
     try:
@@ -97,7 +114,7 @@ else:
                 urls_list.append(path)
     except IOError:
         print(Back.RED + "[-] Error opening wordlist file. Please check if it is downloaded while git cloning.")
-        print(Fore.YELLOW + "EXITING NOW...")
+        print(Fore.YELLOW + "[!] EXITING NOW...")
         quit()
 
 # Scanning for Target
@@ -112,7 +129,7 @@ def scanner(urls_list):
                 print(Fore.GREEN + "[+] Admin Panel found: ", url)
                 output.append(url)
             elif req == 302:
-                print(Back.GREEN + "[+] Redirection on url: ", url)
+                print(Back.GREEN + Style.BRIGHT + "[+] Redirection on url: ", url)
                 output.append(url)
             else:
                 print(Fore.RED + "[-] ",url)
@@ -122,7 +139,8 @@ def scanner(urls_list):
 # Fast Scanning
 fast = args.fast
 if fast:
-    p = len(urls_list)//10
+    print(Fore.BLUE + "[*] Fast Mode ON...")
+    p = len(urls_list)//5
     division1 = urls_list[:p]
     division2 = urls_list[p:2*p]
     division3 = urls_list[2*p:3*p]
@@ -161,8 +179,27 @@ if fast:
     thread3.join()
     thread4.join()
     thread5.join()
+
 else:
-    scanner(urls_list)
+    print(Fore.BLUE + "[*] Normal Mode ON...")
+    p = len(urls_list)//2
+    division1 = urls_list[:p]
+    division2 = urls_list[p:]
+
+    def part1():
+        scanner(division1)
+
+    def part2():
+        scanner(division2)
+
+    thread1 = threading.Thread(target = part1)
+    thread2 = threading.Thread(target = part2)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
 
 # Output File
 outfile = args.outfile
